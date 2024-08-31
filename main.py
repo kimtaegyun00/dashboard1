27f0773c9a5b92925792e8d2fb33677c43823e9f
kimtaegyun00 <countsoot23@gmail.com>
2024-08-31 14:36:45 +0900

    날짜 범위 선택 expander로 교체

--- main.py
+++ main.py
@@ -13,10 +13,18 @@ def main():
     start_date = st.date_input("시작일", df['Date'].min())
     end_date = st.date_input("종료일", df['Date'].max())
 
-    ranged_df = df[(df['Date'] >= pd.to_datetime(start_date))
+df['Date'] = pd.to_datetime(df['Date'])
+with st.expander("범위를 선택하세요"):
+    col1, col2 = st.columns(2)
+    with col1:
+        start_date = st.date_input("시작일", df['Date'].min())
+    with col2:
+        end_date = st.date_input("종료일", df['Date'].max())
+
+ranged_df = df[(df['Date'] >= pd.to_datetime(start_date))
                & (df['Date'] <= pd.to_datetime(end_date))]
-    ranged_df = ranged_df.reset_index(drop=True)
-    st.table(ranged_df)
+ranged_df = ranged_df.reset_index(drop=True)
+st.table(ranged_df)
 
 if __name__ == '__main__' :
     main()
